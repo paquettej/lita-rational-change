d7c6b859b0b061f58dba3505c9711b4c41c8d12b
Jeff Paquette <jeff@snowmoonsoftware.com>
2015-03-21 00:17:08 -0400

    build url to cr

--- lib/lita/handlers/rational_change.rb
+++ lib/lita/handlers/rational_change.rb
@@ -1,11 +1,13 @@
 module Lita
   module Handlers
     class RationalChange < Handler
-      route %r{(.{0,3}#\d+4)}, :build_cr_link
+      route %r{(\S{0,3}#\d+4)}, :build_cr_link
       
       # build a link to ratus1 here
       def build_cr_link(response)
-        response.reply(response.matches)
+        url = "http://10.2.241.182:8600/change/PTweb?ACTION_FLAG=frameset_form&TEMPLATE_FLAG=ProblemReportView&database=%2Fcm%2Fccmdb%2Feverest&role=User&problem_number="
+        url += CGI::escape(response)
+        response.reply("CR #{matches} can be viewed at #{url}")
       end
       
     end

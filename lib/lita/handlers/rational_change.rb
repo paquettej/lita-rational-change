1716e8c931b390aba94b6ebd3127a82c5658ec00
Jeff Paquette <jeff@snowmoonsoftware.com>
2015-03-21 21:46:10 -0400

    resolved template issue with variables, and config settings

--- lib/lita/handlers/rational_change.rb
+++ lib/lita/handlers/rational_change.rb
@@ -2,20 +2,15 @@ module Lita
   module Handlers
     class RationalChange < Handler
       
-      route %r{(\S{0,3}#\d+)}i, :build_cr_link
-
-      attr_accessor  :base_url
+      config :base_url, type: String, required: true
       
-      def self.default_config(config)
-        self.base_url = config.base_url
-      end
+      route %r{(\S{0,3}#\d+)}i, :build_cr_link
             
       # build a link to ratus1 here
       def build_cr_link(response)
         cr = response.matches.first.first
-        url = self.base_url + CGI::escape(cr)
-        response.reply(render_template('cr', locals: {cr: cr, url: url}))
-#        response.reply("CR #{cr} can be viewed at #{url}")
+        url = config.base_url + CGI::escape(cr)
+        response.reply(render_template('cr', cr: cr, url: url))
       end
       
     end

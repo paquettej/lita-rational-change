feb30855c64fd67469f50b87888bf913388bbc9d
Jeff Paquette <jeff@snowmoonsoftware.com>
2015-03-21 21:14:20 -0400

    Change url is now a config item

--- lib/lita/handlers/rational_change.rb
+++ lib/lita/handlers/rational_change.rb
@@ -1,14 +1,20 @@
 module Lita
   module Handlers
     class RationalChange < Handler
-      route %r{(\S{0,3}#\d+)}, :build_cr_link
       
+      route %r{(\S{0,3}#\d+)}, :build_cr_link
+
+      attr_accessor  :base_url
+      
+      def self.default_config(config)
+        self.base_url = config.base_url
+      end
+            
       # build a link to ratus1 here
       def build_cr_link(response)
-        url = "http://10.2.241.182:8600/change/PTweb?ACTION_FLAG=frameset_form&TEMPLATE_FLAG=ProblemReportView&database=%2Fcm%2Fccmdb%2Feverest&role=User&problem_number="
         cr = response.matches.first.first
-        url += CGI::escape(cr)
-        response.reply(render_template('cr', cr: cr, url: url))
+        url = self.base_url + CGI::escape(cr)
+        response.reply(render_template('cr', locals: {cr: cr, url: url}))
 #        response.reply("CR #{cr} can be viewed at #{url}")
       end
       

e3f66e798d709025933dce4a65907905e36b7d3c
Jeff Paquette <jeff@snowmoonsoftware.com>
2015-10-15 13:14:10 -0400

    Remove slack template. Use chat api for slack messages instead.

--- lib/lita/handlers/rational_change.rb
+++ lib/lita/handlers/rational_change.rb
@@ -1,20 +1,48 @@
-module Lita
-  module Handlers
-    class RationalChange < Handler
-      
-      config :base_url, type: String, required: true
-      
-      route %r{(\S{0,3}#\d+)}i, :build_cr_link
-            
-      # build a link to ratus1 here
-      def build_cr_link(response)
-        cr = response.matches.first.first
-        url = config.base_url + CGI::escape(cr)
-        response.reply(render_template('cr', cr: cr, url: url))
-      end
-      
-    end
+module Lita 
+  module Handlers 
+    class RationalChange < Handler 
 
-    Lita.register_handler(RationalChange)
-  end
+      config :base_url, type: String, required: true 
+
+      route %r{(\S{0,3}#\d+)}i, :build_cr_link 
+
+      # build a link to ratus1 here 
+      def build_cr_link(response) 
+        cr_list = response.matches.flatten 
+
+        body_text = build_links(cr_list) 
+
+        case robot.config.robot.adapter 
+        when :slack 
+          target = response.message.source.room_object || response.message.source.user 
+          robot.chat_service.send_attachment(target, build_attachment(body_text)) 
+        else 
+          response.reply(render_template('cr', cr: cr, url: url)) 
+        end 
+      end 
+
+      def build_links(cr_list) 
+        cr_list.map{|cr| format_url(cr)}.join(', ') 
+      end 
+
+      def format_url(cr) 
+        "<#{build_url(cr)}|#{cr}>" 
+      end 
+
+      def build_url(cr) 
+        config.base_url + CGI::escape(cr) 
+      end 
+
+      def build_attachment(text) 
+        Lita::Adapters::Slack::Attachment.new(text, 
+        { 
+          :color => 'danger', 
+          :title => 'Recently mentioned defects', 
+          :thumb_url => 'http://i.imgur.com/Z5HdCsT.png', 
+        }) 
+      end 
+    end 
+
+    Lita.register_handler(RationalChange) 
+  end 
 end
